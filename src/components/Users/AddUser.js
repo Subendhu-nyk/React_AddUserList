dc96dc8486471a61a38a2ac7a8a9c3588a52ea21
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-10-14 23:11:56 +0530

    useEffect

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import Card from '../UI/Card'
 import classes from './AddUser.module.css'
 import Button from '../UI/Button';
@@ -10,29 +10,54 @@ const AddUser=(props)=>{
     const [enteredAge,setEnteredAge]=useState('')
     const [enteredCollegeName,setEnteredCollegeName]=useState('')
     const [error, setError] = useState();
+    const [isFormValid, setIsFormValid] = useState(true);
 
+    useEffect(() => {      
+      setIsFormValid(enteredCollegeName.trim().length !== 0);
+    }, [enteredCollegeName]);
 
-    const addUserHandler=(event)=>{    
-    event.preventDefault();
-    if(enteredUsername.trim().length===0||enteredAge.trim().length===0||enteredCollegeName.trim().length===0){
-        setError({
-            title: 'Invalid input',
-            message: 'Please enter a valid name and age (non-empty values).',
-          });
-        return
-    }
-    if(+enteredAge<1){
-        setError({
-            title: 'Invalid age',
-            message: 'Please enter a valid age (> 0).',
-          });
-        return 
-    }
-    props.onAddUser(enteredUsername,enteredAge,enteredCollegeName)
-    setEnteredAge('')
-    setEnteredUsername('')
-    setEnteredCollegeName('')
-}
+//     const addUserHandler=(event)=>{    
+//     event.preventDefault();
+//     if(enteredUsername.trim().length===0||enteredAge.trim().length===0||enteredCollegeName.trim().length===0){
+//         setError({
+//             title: 'Invalid input',
+//             message: 'Please enter a valid name and age (non-empty values).',
+//           });
+//         return
+//     }
+//     if(+enteredAge<1){
+//         setError({
+//             title: 'Invalid age',
+//             message: 'Please enter a valid age (> 0).',
+//           });
+//         return 
+//     }
+//     props.onAddUser(enteredUsername,enteredAge,enteredCollegeName)
+//     setEnteredAge('')
+//     setEnteredUsername('')
+//     setEnteredCollegeName('')
+// }
+const addUserHandler = (event) => {
+  event.preventDefault();
+  if (
+    enteredUsername.trim().length === 0 ||
+    enteredAge.trim().length === 0 ||
+    !isFormValid ||
+    +enteredAge < 1
+  ) {
+    setError({
+      title: 'Invalid input',
+      message: 'Please enter valid values for all fields.',
+    });
+    return;
+  }
+
+  props.onAddUser(enteredUsername, enteredAge, enteredCollegeName);
+  setEnteredAge('');
+  setEnteredUsername('');
+  setEnteredCollegeName('');
+  setError(null);  // Clearing the error state
+};
 const usernameChangeHandler=(event)=>{
 setEnteredUsername(event.target.value)
 }
